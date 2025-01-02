f33c9e81882ff3b3668460eb0a0586beded23896
47lucid <vivekshakya673@gmail.com>
2025-01-02 18:53:59 +0530

    LTS: Update to 4.14.337

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Dec 18 04:44:10 IST 2024"
+#define LINUX_COMPILE_BY "Lucid_|For•6785"
+#define LINUX_COMPILE_HOST "Lucid"
+#define LINUX_COMPILER "ZyC clang version 20.0.0git (https://github.com/llvm/llvm-project bf700c39d1c9f88387bfe2ec4567567397200ea3), LLD 20.0.0 (https://github.com/llvm/llvm-project bf700c39d1c9f88387bfe2ec4567567397200ea3)"
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.14.337-Bolt-V4-Revival+"
